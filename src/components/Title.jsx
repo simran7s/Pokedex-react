cefa20538e7020fb20be5bf9e822eab99e18dd18
Simran Sandhu <simran7s@my.yorku.ca>
2021-10-02 22:15:48 -0400

    setup react project

---
+++ src/components/Title.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Title() {
+    return (
+        <header className="bg-primary text-center text-light">
+            <h1 className="title">Pokedex</h1>
+        </header>
+    )
+}
+
+export default Title
